a4bf50bb01fa08a3f442a6260377c276579d9f89
Wang Da - Mech <48291391+WangDaMME@users.noreply.github.com>
2021-10-21 08:48:56 -0400

    Assignment_2 AboutUs Page

--- react/confusion/src/components/DishdetailComponent.jsx
+++ react/confusion/src/components/DishdetailComponent.jsx
@@ -22,87 +22,78 @@ import {Link} from "react-router-dom"
 
 // DishDetail 是 presentional component 可以 改成 函数式组件
 
-function RenderComments({comments}) {
+function RenderComments({ comments }) {
     if (comments == null) {
-        return (<div></div>)
+        return <div></div>;
     }
-    const remarks = comments.map(comment => {
+    const remarks = comments.map((comment) => {
         return (
             <li key={comment.id}>
                 <p>{comment.comment}</p>
-                <p>-- {comment.author},
-                &nbsp;
-                {new Intl.DateTimeFormat('en-US', {
+                <p>
+                    -- {comment.author}, &nbsp;
+                    {new Intl.DateTimeFormat('en-US', {
                         year: 'numeric',
                         month: 'long',
-                        day: '2-digit'
+                        day: '2-digit',
                     }).format(new Date(comment.date))}
                 </p>
             </li>
-        )
-    })
+        );
+    });
     return (
         <div className='col-12 col-md-5 m-1'>
             <h4> Comments </h4>
-            <ul className='list-unstyled'>
-                {remarks}
-            </ul>
+            <ul className='list-unstyled'>{remarks}</ul>
         </div>
-    )
+    );
 }
 
-function RenderDish({dish}) {
+function RenderDish({ dish }) {
     if (dish != null) {
         return (
             <div className='col-12 col-md-5 m-1'>
                 <Card>
-                    <CardImg width="100%" src={dish.image} alt={dish.name} />
+                    <CardImg top src={dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
                     </CardBody>
                 </Card>
             </div>
-        )
-    }
-    else {
-        return (<div></div>)
+        );
+    } else {
+        return <div></div>;
     }
 }
 
-const DishDetail=(props)=>{
-    if(props.dish!=null)
-    {
+const DishDetail = (props) => {
+    if (props.dish != null)
         return (
-            <div className="container">
-                <div className="row">
+            <div className='container'>
+                <div className='row'>
                     <Breadcrumb>
-
-                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        <BreadcrumbItem>
+                            <Link to='/menu'>Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            {props.dish.name}
+                        </BreadcrumbItem>
                     </Breadcrumb>
-                    <div className="col-12">
+                    <div className='col-12'>
                         <h3>{props.dish.name}</h3>
                         <hr />
-                    </div>                
+                    </div>
                 </div>
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderDish dish={props.dish} />
-                    </div>
-                    <div className="col-12 col-md-5 m-1">
-                        <RenderComments comments={props.comments} />
-                    </div>
+                <div className='row'>
+                    <RenderDish dish={props.dish} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
-
+    else {
+        return <div></div>;
     }
-    else{
-        return <div></div>
-    }
-}
-
-
+};
 
 export default DishDetail;
--- react/confusion/src/components/MainComponent.js
+++ react/confusion/src/components/MainComponent.js
@@ -11,6 +11,7 @@ import {PROMOTIONS} from "../shared/promotions";
 import Home from './HomeComponent';
 import {Switch, Route, Redirect} from "react-router-dom"
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 
 class Main extends Component {
 
@@ -56,6 +57,7 @@ class Main extends Component {
 
         <Switch>
           <Route path="/home" component={HomePage} />
+          <Route exact path="/aboutus" component={ ()=> <About leaders = {this.state.leaders}/> } /> {/* pass the props to a component */}
           <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} /> {/* pass the props to a component */}
           <Route exact path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
