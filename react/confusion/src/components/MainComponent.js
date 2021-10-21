81356a678c96ca3fa23c7d80c7ee64c10fd03d04
Wang Da - Mech <48291391+WangDaMME@users.noreply.github.com>
2021-10-20 23:48:49 -0400

    Add files via upload

--- react/confusion/src/components/MainComponent.js
+++ react/confusion/src/components/MainComponent.js
@@ -4,6 +4,13 @@ import DishDetail from './DishdetailComponent';
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import {DISHES} from "../shared/dishes";
+import {COMMENTS} from "../shared/comments";
+import {LEADERS} from "../shared/leaders";
+import {PROMOTIONS} from "../shared/promotions";
+
+import Home from './HomeComponent';
+import {Switch, Route, Redirect} from "react-router-dom"
+import Contact from './ContactComponent';
 
 class Main extends Component {
 
@@ -11,21 +18,52 @@ class Main extends Component {
     super(props);
     this.state = {
         dishes: DISHES,
-        selectedDish: null
-    };
+        comments: COMMENTS,
+        promotions: PROMOTIONS,
+        leaders : LEADERS
+      };
   }
 
-  onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId});
-  }
+  // onDishSelect(dishId) {
+  //   this.setState({ selectedDish: dishId});
+  // }
 
   render() {
+
+    const HomePage = ()=>{
+      return (
+        //{/*array 返回 index=0*/}
+        <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
+
+      );
+    }
+
+    //路由组建 props 有3大属性 match,location history
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+        comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+      );
+    };
+    
     return (
       <div>
         <Header/>
 
-        <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
-        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Switch>
+          <Route path="/home" component={HomePage} />
+          <Route exact path="/menu" component={()=> <Menu dishes={this.state.dishes}/>} /> {/* pass the props to a component */}
+          <Route exact path="/menu/:dishId" component={DishWithId} />
+          <Route exact path="/contactus" component={Contact} />
+          <Redirect to="/home" /> {/* default if not matching*/}
+        </Switch>
+
+        {/* <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} /> */}
         <Footer />
       </div>
     );
--- react/confusion/src/components/MenuComponent.js
+++ react/confusion/src/components/MenuComponent.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-
+import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import {Link} from "react-router-dom";
 
 // Menu 是 presentional component 可以 改成 函数式组件
 
 function RenderMenuItem({dish, onClick}) // props 可以写成 形参 形式 props 只是个 js object   onClick 函数形式
 {
     return (
-        <Card onClick={()=>onClick(dish.id)} >
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-                <CardTitle >{dish.name}</CardTitle>
-            </CardImgOverlay>
+        <Card>
+            <Link to ={`/menu/${dish.id}`}>
+
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle >{dish.name}</CardTitle>
+                </CardImgOverlay>
+            </Link>
         </Card>    
     )
 }
@@ -20,12 +23,24 @@ const Menu = (props)=>{
     const menu = props.dishes.map(dish => {
         return (
             <div key={dish.id} className='col-12 col-md-5 m-1'>
-               <RenderMenuItem dish={dish} onClick={props.onClick}/>
+               <RenderMenuItem dish={dish}/>
             </div>
         );
     });
     return (
         <div className='container'>
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem>
+                        <Link to="/home">Home</Link>
+                    </BreadcrumbItem>
+                    <BreadcrumbItem active>Menu</BreadcrumbItem>
+                </Breadcrumb>
+                <div className="col-12">
+                    <h3>Menu</h3>
+                    <hr />
+                </div>
+            </div>
             <div className='row'>
                 {menu}
             </div>
