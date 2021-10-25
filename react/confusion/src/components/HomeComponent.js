9346e9d1e2709a0a59192baba29f759ed7ce4a94
Wang Da - Mech <48291391+WangDaMME@users.noreply.github.com>
2021-10-25 11:43:53 -0400

    Add slide animation for Menu & Pop animation for Home

--- react/confusion/src/components/HomeComponent.js
+++ react/confusion/src/components/HomeComponent.js
@@ -10,6 +10,9 @@ import {
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
 
+import { FadeTransform } from 'react-animation-components';
+
+
 function RenderCard({ item, isLoading, errMess }) {
     if (isLoading) {
         return <Loading />;
@@ -17,16 +20,21 @@ function RenderCard({ item, isLoading, errMess }) {
         return <h4>{errMess}</h4>;
     } else
         return (
-            <Card>
-                <CardImg src={baseUrl + item.image} alt={item.name} />
-                <CardBody>
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
                     <CardTitle>{item.name}</CardTitle>
-                    {item.designation ? (
-                        <CardSubtitle>{item.designation}</CardSubtitle>
-                    ) : null}
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
                     <CardText>{item.description}</CardText>
-                </CardBody>
-            </Card>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
+            
         );
 }
 
